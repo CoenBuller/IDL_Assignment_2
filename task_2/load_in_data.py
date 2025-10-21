0168e67ddc1669846c3d4eb629d3356e3994a1ad
Coen Büller <coen.buller@gmail.com>
2025-10-21 22:02:46 +0200

    Small change to CNN code and added a python file that can be used to download the data on the lab computers. Pls make shure that you dont commit the data to your branch

---
+++ task_2/load_in_data.py
@@ -0,0 +1,14 @@
+"""This code can be used to load in the data from the website"""
+
+import requests
+import io
+import zipfile
+
+
+#The data comes in two resolutions, one of 75x75 img. and one of 150x150 img.
+url_75 = 'https://surfdrive.surf.nl/index.php/s/Nznt5c48Mzlb2HY/download?path=%2F&files=A1_data_75.zip'
+url_150 = 'https://surfdrive.surf.nl/index.php/s/Nznt5c48Mzlb2HY/download?path=%2F&files=A1_data_150.zip'
+
+r = requests.get(url=url_150) #Specify which data you want to load in
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall("./task_2/data_150") #Specify which directory to store the data in 
